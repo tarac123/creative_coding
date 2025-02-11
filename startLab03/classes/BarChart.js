2eb023bf9bf6a606ce7837b565801a86a56eaeb7
Tara Carroll (Student) <N00230895@iadt.ie>
2025-02-11 15:02:01 +0000

    ticksworking

--- startLab03/classes/BarChart.js
+++ startLab03/classes/BarChart.js
@@ -1,85 +1,119 @@
 class BarChart {
-    constructor(_data,_xValue,_yValue,_chartHeight,_chartWidth,_barWidth,_margin,_scaler,_axisThickness,_chartPosX,_chartPosY,_axisColour,_barColor,_axisTextColour){
+    constructor(obj){
         
-        this.data = _data;
-        this.xValue = _xValue;
-        this.yValue = _yValue;
-        this.chartHeight = _chartHeight;
-        this.chartWidth = _chartWidth;
-        this.barWidth =_barWidth;
-        this.margin = _margin;
-        this.axisThickness = _axisThickness;
-        this.chartPosX = _chartPosX;
-        this.chartPosY = _chartPosY;
+        this.data = obj.data;
+        this.xValue = obj.xValue;
+        this.yValue = obj.yValue;
+        this.chartHeight = obj.chartHeight || 500;
+        this.chartWidth = obj.chartWidth || 500;
+        this.barWidth = obj.barWidth || 30;
+        this.margin = obj.margin || 10;
+        this.axisThickness = obj.axisThickness || 1;
+        this.chartPosX = obj.chartPosX || 50;
+        this.chartPosY = obj.chartPosY || 600;
+
 
         this.gap = (this.chartWidth - (this.data.length * this.barWidth ) - (this.margin*2)) / (this.data.length - 1);
         this.scaler = this.chartHeight / (max(cleanedData.map(row => row[this.yValue])));
 
         this.axisColour = color(0,0,0);
-        this.barColor = color(135, 226, 35, 100);
+        this.barColor = color(128, 235, 52, 100);
         this.axisTextColour= color(0);
+
+        this.numTicks=5;
+        this.tickLength=10;
     }
 
 
-
-
-        renderBars(){
-
-        }
-
-        renderAxis(){
-
-        }
-
-        renderTicks(){
-            push()
+    renderBars(){
+        push()
             translate(this.chartPosX,this.chartPosY)
             noFill()
             stroke(this.axisColour)
             strokeWeight(this.axisThickness)
-            
-            for (let i=0; i< this.numTicks; i++){
-                line (0,0,-10,0)
+            line (0,0,0,-this.chartHeight) //vertical line
+            line (0,0,this.chartWidth,0) //horizontal line
+     
+     
+                push()
+                    translate(this.margin,0)
+                    for(let i = 0; i<this.data.length; i++){
+                        let xPos = (this.barWidth + this.gap) * i;
+                        fill(this.barColor)
+                        rect (xPos, 0, this.barWidth, -this.data[i][this.yValue]*this.scaler)
+     
+                        // fill(this.axisTextColour)
+                        // noStroke()
+                        // textAlign(LEFT, CENTER)
+                        // textSize (8)
+                        // push()
+                        // translate(xPos + (this.barWidth/2),20)
+                        // rotate(45)
+                        // text (this.data[i][this.xValue],0,0);
+                        // pop()
+                    }
+                    pop()
+                    pop()
+        }
+     
+        renderAxis(){
+                push()
+                    translate(this.chartPosX,this.chartPosY)
+                    noFill()
+                    stroke(this.axisColour)
+                    strokeWeight(this.axisThickness)
+                    line (0,0,0,-this.chartHeight) //vertical line
+                    line (0,0,this.chartWidth,0) //horizontal line    
+                   
+                    pop()
+                }
+     
+     
+     
+        renderLabels(){
+            push()
+                translate(this.chartPosX,this.chartPosY)
+       
+       
+                    push()
+                        translate(this.margin,0)
+                        for(let i = 0; i<this.data.length; i++){
+                            let xPos = (this.barWidth + this.gap) * i;
+     
+                            fill(this.axisTextColour)
+                        noStroke()
+                        textAlign(LEFT, CENTER)
+                        textSize (8)
+                        push()
+                        translate(xPos + (this.barWidth/2),20)
+                        rotate(45)
+                        text (this.data[i][this.xValue],0,0);
+                        pop()
+     
+                        }
+            pop()    
+            pop()        
             }
-
-            pop()
-        }
-        render(){
-        
-    //         push()
-    //         translate(this.chartPosX,this.chartPosY)
-    //         noFill()
-    //         stroke(this.axisColour)
-    //         strokeWeight(this.axisThickness)
-    //         line (0,0,0,-this.chartHeight) // x axis
-    //         line (0,0,this.chartWidth,0) //y axis
-        
-        
-    //         push()
-    //         translate(this.margin,0)
-    //         for(let i = 0; i < this.data.length; i++){
-    //             let xPos = (this.barWidth + this.gap)* i;
-    //             fill(this.barColor)
-    //             rect (xPos, 0, this.barWidth, -this.data[i][this.yValue]*this.scaler)
+     
+            renderTicks() {
+                push();
+                translate(this.chartPosX, this.chartPosY);
+                noFill();
+                stroke(this.axisColour);
+                strokeWeight(this.axisThickness);
                 
-    //             fill(this.axisTextColour)
-    //             noStroke()
-    //             textAlign(LEFT)
-    //             textSize (9)
-    //             push()
-    //             translate(xPos + (this.barWidth/2),15)
-    //             rotate(45)
-    //             text (this.data[i][this.xValue],10,0);
-    //             pop()
-            
-            
-    //         }
-    //     pop()
-        
-    // pop()
-        
-
-        }
+                let tickIncrement = this.chartHeight / this.numTicks;
+                
+                for (let i = 0; i <= this.numTicks; i++) { 
+                    let yPos = -tickIncrement * i;
+                    line(0, yPos, -this.tickLength, yPos);
+                }
+                
+                pop();
+            }
+                   
     }
+
+  
 
 
--- startLab03/sketch.js
+++ startLab03/sketch.js
@@ -7,21 +7,44 @@ function preload(){
     data = loadTable('data/Combined.csv','csv','header')
 }
  
-function setup(){
-    createCanvas(1050, 1050);
+function setup() {
+    createCanvas(1000, 1000);
     angleMode(DEGREES);
     noLoop();
+
     cleanData();
-    charts.push(new BarChart(cleanedData,"Age_Group","Female",400,400,25,10,10,3,50,450))
-    charts.push(new BarChart(cleanedData,"Age_Group","Male",400,400,25,10,10,3,50,950))
-    charts.push(new BarChart(cleanedData,"Age_Group","Total",400,400,25,10,10,3,550,450))
-    
+
+    charts.push(new BarChart({
+        data: cleanedData,
+        xValue: "Age_Group",
+        yValue: "Female",
+        // chartHeight: 400,
+        // chartWidth: 400,
+        // barWidth: 25,
+        // margin: 10,
+        // axisThickness: 10,
+        // xPos: 3,
+        // yPos: 50
+    }
+    ))
 }
+
+    
+    // charts.push(new BarChart(cleanedData,"Age_Group","Male",400,400,25,10,10,3,50,950))
+    // charts.push(new BarChart(cleanedData,"Age_Group","Total",400,400,25,10,10,3,550,450))
+    
+
  
 function draw(){
-    background(210)
-    charts.forEach(chart => chart.render())
+    background(190)
+    charts.forEach(chart => {
+        chart.renderBars();
+        chart.renderLabels();
+        chart.renderAxis();
+        chart.renderTicks();
+    })
 }
+
  
 function cleanData(){
     for(let i=0; i<data.rows.length; i++){
