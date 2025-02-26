254fecea4f9983be2f5b8dea8b259d5f619ee8a4
tara carroll <n00230895@iadt.ie>
2025-02-26 10:25:16 +0000

    submission

--- CA1 home files/CA1/PieChart.js
+++ CA1 home files/CA1/PieChart.js
@@ -7,9 +7,9 @@ class PieChart {
         this.outerData = obj.outerData; // Top 3 outer values
         this.innerData = obj.innerData; // Top 3 inner values
         
-        //  label names for outer and inner data
-        this.outerLabels = obj.outerLabels || [];
-        this.innerLabels = obj.innerLabels || [];
+        //  label names for outer and inner data 
+        this.outerLabels = obj.outerLabels || []; //tempo
+        this.innerLabels = obj.innerLabels || []; //valence
         
         this.outerColors = [color(7, 15, 99), color(14, 28, 173), color(17, 37, 245)]; // Colors for outer pie
         this.innerColors = [color(93, 108, 252), color(140, 150, 250), color(189, 195, 252)]; // Colors for inner pie
@@ -21,10 +21,13 @@ class PieChart {
         this.innerTitle = obj.innerTitle || "Inner";
     }
 
+
+
     renderPie() {
         push();
         translate(this.chartPosX, this.chartPosY);
-        noStroke();
+        stroke(255); // White stroke on outer and inner
+        strokeWeight(2);
 
         // Calculate total sum beforehand
         let totalOuter = sum(this.outerData);
@@ -32,20 +35,20 @@ class PieChart {
 
         // Draw outer pie chart
         let lastAngle = 0;
-        for (let i = 0; i < this.outerData.length; i++) {
-            let angle = map(this.outerData[i], 0, totalOuter, 0, 360);
-            fill(this.outerColors[i]);
-            arc(0, 0, this.chartHeight, this.chartHeight, lastAngle, lastAngle + angle, PIE);
-            lastAngle += angle;
+        for (let i = 0; i < this.outerData.length; i++) { //iterates through outer data
+            let angle = map(this.outerData[i], 0, totalOuter, 0, 360); // calculates segment for each value 
+            fill(this.outerColors[i]);  
+            arc(0, 0, this.chartHeight, this.chartHeight, lastAngle, lastAngle + angle, PIE); //draws pie segments
+            lastAngle += angle; //increments angle for next segement
         }
 
         // Draw inner pie chart
         lastAngle = 0;
-        for (let i = 0; i < this.innerData.length; i++) {
+        for (let i = 0; i < this.innerData.length; i++) { 
             let angle = map(this.innerData[i], 0, totalInner, 0, 360);
             fill(this.innerColors[i]);
-            arc(0, 0, this.chartHeight * 0.6, this.chartHeight * 0.6, lastAngle, lastAngle + angle, PIE);
-            lastAngle += angle;
+            arc(0, 0, this.chartHeight * 0.6, this.chartHeight * 0.6, lastAngle, lastAngle + angle, PIE); //inner pie doees the  same thing but at 60% of the size of outer pie 
+            lastAngle += angle; 
         }
         pop();
     }
@@ -63,10 +66,10 @@ class PieChart {
     
     renderLegend() {
         push();
-        const legendX = this.chartPosX + this.chartHeight / 2 + 20;
-        const legendY = this.chartPosY - this.chartHeight / 3;
-        const rectSize = 15;
-        const lineHeight = 25;
+        let legendX = this.chartPosX + this.chartHeight / 2 + 20;
+        let legendY = this.chartPosY - this.chartHeight / 3;
+        let rectSize = 15;
+        let lineHeight = 25;
         
         textAlign(LEFT, CENTER);
         textSize(12);
@@ -89,7 +92,7 @@ class PieChart {
         }
     
         // Title for inner data (with spacing)
-        const innerLegendY = legendY + (this.outerLabels.length + 1) * lineHeight;
+        let innerLegendY = legendY + (this.outerLabels.length + 1) * lineHeight;
         fill(0);
         textStyle(BOLD);
         text(this.innerTitle, legendX, innerLegendY - lineHeight);
@@ -105,11 +108,12 @@ class PieChart {
                  legendX + rectSize + 5, 
                  innerLegendY + i * lineHeight + rectSize / 2);
         }
-    
+
         pop();
     }
 }
 
 function sum(arr) {
-    return arr.reduce((a, b) => a + b, 0);
+    return arr.reduce((a, b) => a + b, 0); //sums up all values in an array to get total to work out segment sizes
 }
+
